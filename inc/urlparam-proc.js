554d66bc5562b6acdb781083f6d57eaa4cd1d70b
Brenno Nascimento <brenno.nascimento@saudeid.com.br>
2022-03-07 13:50:18 -0300

    feat: add layers silently

--- inc/urlparam-proc.js
+++ inc/urlparam-proc.js
@@ -28,9 +28,9 @@ if (mrgMarkerLatLonByURL !=null){
 //Os parâmetros podem ser passados de forma mais estruturada... verificar depois
 
 //INFO: mrgAddDataOverlay(Pasta,Arquivo,Apelido,IconDefault,IconMini,Enquadrar,Heat,Cluster)
-mrgAddDataOverlay('global', 'perimetro-pe', 'Pernambuco', null, 'vector-square', !(ForceSilentAddOverlay), HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
-mrgAddDataOverlay('global', 'perimetro-recife', 'Recife', null, 'vector-square', !(ForceSilentAddOverlay), HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
-mrgAddDataOverlay('global','bairros-recife','Bairros Recife',null,'vector-square',!(ForceSilentAddOverlay),HeatOFF,ClusterOFF,OvCountOFF,OvExibirOn);
+mrgAddDataOverlay('global', 'perimetro-pe', 'Pernambuco', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
+mrgAddDataOverlay('global','bairros-recife','Bairros Recife',null,'vector-square',ForceSilentAddOverlay,HeatOFF,ClusterOFF,OvCountOFF,OvExibirOn);
+mrgAddDataOverlay('global', 'perimetro-recife', 'Recife', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
 
 //mrgAddDataOverlay('comercio','comercio','POIs',null,'vector-square',!(ForceSilentAddOverlay),HeatOFF,true,OvCountOFF,OvExibirOn); 
 //mrgAddDataOverlay('global','vias','Ruas','road','road',false,HeatOFF,ClusterON,OvCountOFF); 
