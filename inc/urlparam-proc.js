9f2bbba1499fcb3b9c36648d0f6de5f66a48a6a3
Brenno Nascimento <brenno.nascimento@saudeid.com.br>
2022-03-08 13:52:16 -0300

    chores: clean identation style

--- inc/urlparam-proc.js
+++ inc/urlparam-proc.js
@@ -1,55 +1,55 @@
 //Processamento de parâmentros via URL
 
 
-if (mrgMarkerByURL){
-	var Center = map.getCenter();
-	mrgMarkerLatLonByURL = [Center.lat, Center.lng];	
+if (mrgMarkerByURL) {
+  var Center = map.getCenter()
+  mrgMarkerLatLonByURL = [Center.lat, Center.lng]
 }
 
 var ForceSilentAddOverlay = false //Força para que overlayers sejam adicionadas sem mudar a tela do mapa (dev)
 //Se foi solicitado marcador no mapa
 
-if (mrgMarkerLatLonByURL !=null){ 	
-	ForceSilentAddOverlay = true; //Nesse caso desabilita para que seja focado no marcador em questão
-	if (!mrgMapOnClickAddLock){
-		mrgMapOnClickAddLock = true;
-		mrgUserTempMarker.setLatLng(mrgMarkerLatLonByURL);
-		map.setView(mrgMarkerLatLonByURL, 18); 		
-		var Msg = TempMarkerMsg(mrgMarkerLatLonByURL[0],mrgMarkerLatLonByURL[1],17);
-		mrgUserTempMarker.bindPopup(Msg).addTo(map); 
-		mrgUserTempMarker.openPopup(); 
-	}else{
-		mrgMapOnClickAddLock = false;
-		map.removeLayer(mrgUserTempMarker);
-	}		
+if (mrgMarkerLatLonByURL !=null) {
+  ForceSilentAddOverlay = true //Nesse caso desabilita para que seja focado no marcador em questão
+  if (!mrgMapOnClickAddLock) {
+    mrgMapOnClickAddLock = true
+    mrgUserTempMarker.setLatLng(mrgMarkerLatLonByURL)
+    map.setView(mrgMarkerLatLonByURL, 18)
+    var Msg = TempMarkerMsg(mrgMarkerLatLonByURL[0],mrgMarkerLatLonByURL[1],17)
+    mrgUserTempMarker.bindPopup(Msg).addTo(map)
+    mrgUserTempMarker.openPopup()
+  } else {
+    mrgMapOnClickAddLock = false
+    map.removeLayer(mrgUserTempMarker)
+  }
 }
 
 //INFO: mrgAddDataOverlay(Pasta,Arquivo,Apelido,IconDefault,IconMini,Enquadrar,Heat,Cluster)
 //Os parâmetros podem ser passados de forma mais estruturada... verificar depois
 
 //INFO: mrgAddDataOverlay(Pasta,Arquivo,Apelido,IconDefault,IconMini,Enquadrar,Heat,Cluster)
-mrgAddDataOverlay('global', 'perimetro-pe', 'Pernambuco', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
-mrgAddDataOverlay('global', 'perimetro-recife', 'Recife', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn);
-mrgAddDataOverlay('global','bairros-recife','Bairros Recife',null,'vector-square',ForceSilentAddOverlay,HeatOFF,ClusterOFF,OvCountOFF,OvExibirOff);
+mrgAddDataOverlay('global', 'perimetro-pe', 'Pernambuco', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn)
+mrgAddDataOverlay('global', 'perimetro-recife', 'Recife', null, 'vector-square', ForceSilentAddOverlay, HeatOFF, ClusterOFF, OvCountOFF, OvExibirOn)
+mrgAddDataOverlay('global','bairros-recife','Bairros Recife',null,'vector-square',ForceSilentAddOverlay,HeatOFF,ClusterOFF,OvCountOFF,OvExibirOff)
 
-//mrgAddDataOverlay('comercio','comercio','POIs',null,'vector-square',!(ForceSilentAddOverlay),HeatOFF,true,OvCountOFF,OvExibirOn); 
-//mrgAddDataOverlay('global','vias','Ruas','road','road',false,HeatOFF,ClusterON,OvCountOFF); 
+//mrgAddDataOverlay('comercio','comercio','POIs',null,'vector-square',!(ForceSilentAddOverlay),HeatOFF,true,OvCountOFF,OvExibirOn)
+//mrgAddDataOverlay('global','vias','Ruas','road','road',false,HeatOFF,ClusterON,OvCountOFF)
 
 
-if (mrgCamadaDeDados !=null){ 
-   var OK = true; //DEV
+if (mrgCamadaDeDados !=null) {
+   var OK = true //DEV
    switch( mrgCamadaDeDados  ) {
-	case 'turismo'		   : 
-		mrgAddDataOverlay('turismo','aeb','A & B','utensils','utensils',false,HeatOFF,ClusterOFF,OvCountOFF); 
-		mrgAddDataOverlay('turismo','eventos','Eventos','calendar-alt','calendar-alt',false,HeatOFF,ClusterOFF,OvCountOFF); 
-		mrgAddDataOverlay('turismo','atrativos','Atrativos','map-marked-alt','map-marked-alt',false,HeatOFF,ClusterOFF,OvCountOFF); 
-		mrgAddDataOverlay('turismo','hospedagem','Hospedagem','bed','bed',false,HeatOFF,ClusterOFF,OvCountOFF) 
-	break; 
-	case 'rtcma'		   : 
-		mrgAddDataOverlay('rtcma','rtcma','RT Cavernas da Mata Atlântica','map-marked-alt','map-marked-alt',true,HeatOFF,ClusterOFF,OvCountOFF); 
-	break; 
-    default                        : OK = false;
+  case 'turismo':
+    mrgAddDataOverlay('turismo','aeb','A & B','utensils','utensils',false,HeatOFF,ClusterOFF,OvCountOFF)
+    mrgAddDataOverlay('turismo','eventos','Eventos','calendar-alt','calendar-alt',false,HeatOFF,ClusterOFF,OvCountOFF)
+    mrgAddDataOverlay('turismo','atrativos','Atrativos','map-marked-alt','map-marked-alt',false,HeatOFF,ClusterOFF,OvCountOFF)
+    mrgAddDataOverlay('turismo','hospedagem','Hospedagem','bed','bed',false,HeatOFF,ClusterOFF,OvCountOFF)
+  break
+  case 'rtcma':
+    mrgAddDataOverlay('rtcma','rtcma','RT Cavernas da Mata Atlântica','map-marked-alt','map-marked-alt',true,HeatOFF,ClusterOFF,OvCountOFF)
+  break
+    default: OK = false
    }
-      
-   
+
+
 }
